d96453b5da01e5ca38bedde614b71f0714619bde
Shashank <shashankprabhakar@icloud.com>
2024-10-08 19:04:13 +0530

    Initial Commit

---
+++ RestartApp/RestartAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartAppApp.swift
+//  RestartApp
+//
+//  Created by Shashank Prabhakar on 08/10/24.
+//
+
+import SwiftUI
+
+@main
+struct RestartAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
